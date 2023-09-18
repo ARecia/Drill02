03e49a2c40520e2391350f81ec78ef59410c6523
yunkukim <josh630@tukorea.ac.kr>
2023-09-18 14:24:33 +0900

    캐릭터와 풀 등장

--- character_grass.py
+++ character_grass.py
@@ -6,14 +6,9 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while (x < 800):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    update_canvas()
-    delay(0.01)
-    get_events()
+grass.draw_now(400, 30)
+character.draw_now(400, 90)
+
+delay(5)
 
 close_canvas()
